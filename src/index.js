23b71782af03aa072eabc277ed3d97117ad67be8
LKWLaLa <lkwlala@gmail.com>
2018-02-16 13:46:02 -0500

    add Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import RootReducer from './reducers/restaurantReducer';
 import {addRestaurants} from './actions/restaurantActions';
 import {BrowserRouter as Router} from 'react-router-dom';
@@ -12,8 +13,10 @@ import registerServiceWorker from './registerServiceWorker';
 let store = createStore(RootReducer);
 
 ReactDOM.render((
+<Provider store={store}> 
   <Router>
     <App />
   </Router>
+</Provider>
   ), document.getElementById('root'));
 registerServiceWorker();
