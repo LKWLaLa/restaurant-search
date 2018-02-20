9ffb4c583bec0a4667e4297cded098ee9377d4f5
LKWLaLa <lkwlala@gmail.com>
2018-02-20 17:09:08 -0500

    configure redux-persist

--- src/index.js
+++ src/index.js
@@ -3,23 +3,36 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import RootReducer from './reducers/restaurantReducer';
+import rootReducer from './reducers/restaurantReducer';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web 
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-let store = createStore(RootReducer, compose(
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+let store = createStore(persistedReducer, compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
+let persistor = persistStore(store)
+
 ReactDOM.render((
 <Provider store={store}> 
-  <Router>
-    <App />
-  </Router>
+  <PersistGate loading={null} persistor={persistor}>
+    <Router>
+      <App />
+    </Router>
+  </PersistGate>
 </Provider>
   ), document.getElementById('root'));
 registerServiceWorker();
