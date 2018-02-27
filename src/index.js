0dd9a5cc7be7b117d4424bbd71cdc79bf7f6be6e
LKWLaLa <lkwlala@gmail.com>
2018-02-26 21:14:45 -0500

    remove redux-persist for now

--- src/index.js
+++ src/index.js
@@ -8,31 +8,17 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web 
 
-const persistConfig = {
-  key: 'root',
-  storage,
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-let store = createStore(persistedReducer, compose(
+let store = createStore(rootReducer, compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
-let persistor = persistStore(store)
-
 ReactDOM.render((
 <Provider store={store}> 
-  <PersistGate loading={null} persistor={persistor}>
     <Router>
       <App />
     </Router>
-  </PersistGate>
 </Provider>
   ), document.getElementById('root'));
 registerServiceWorker();
