d80f4b7cebd42dcc6ae64b7ed8256d7ea8fe60d1
LKWLaLa <lkwlala@gmail.com>
2018-03-06 16:59:09 -0500

    remove city from restaurant tile and update styling

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const RestaurantItem = (props) => {
- const {name, streetAddress, city, state, zip, foodTypes, logoUrl} = props.restaurant
+ const {name, streetAddress, city, zip, foodTypes, logoUrl} = props.restaurant
 
   return (
     <div className="rest-tile">
       <img className="rest-icon" src={logoUrl} alt="restaurant logo"/>
       <div className="rest-text">
         <h2>{name}</h2>
-        <p>{streetAddress}, {city}, {state} {zip}</p>
+        <p>{streetAddress}, {city}, {zip}</p>
         <p className="food-types">{foodTypes.slice(0,3).join(', ')}</p>
         <Link to={`restaurants/${props.id}`}>Click here for menu </Link>  
       </div>
