f902b4bef0ef918abbe4a9608308f8627ee8e100
LKWLaLa <lkwlala@gmail.com>
2018-06-07 20:21:55 -0400

    add Router, Provider, and store to App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {createStore} from 'redux';
+import rootReducer from './reducers/restaurantReducer';
+import {Provider} from 'react-redux';
+
+let store = createStore(rootReducer);
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    (<Provider store={store}> 
+      <Router>      
+       <App />      
+      </Router>
+    </Provider>), 
+    div);
   ReactDOM.unmountComponentAtNode(div);
 });
